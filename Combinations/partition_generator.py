325b1b785c730201a236dfee45696b8317a771de
fork52 <mrunankmistry52@gmail.com>
2021-12-13 23:13:25 +0530

    Added partition generator.py

---
+++ Combinations/partition_generator.py
@@ -0,0 +1,32 @@
+
+def partition_generator(arr):
+    """
+    Generates and returns all partitions of an array.
+    Time Complexity:  O( 2 ^ (n - 1) * n)
+    Space Complexity: O( 2 ^ (n - 1) * n)
+    """
+    partitions = []
+    n = len(arr)
+
+    lim = 1 << (n - 1)
+    for mask in range(lim):
+        current_partition = []
+        current_subset = []
+        for ind in range(n):
+            current_subset.append(arr[ind])
+            if mask & (1 << ind) or ind == n - 1:
+                current_partition.append(current_subset)
+                current_subset = []
+        partitions.append(current_partition)
+
+    return partitions
+
+
+if __name__ == '__main__':
+    arr = [1, 2, 3, 4]
+
+    partitions = partition_generator(arr)
+
+    print('The partitions are:')
+    for current_partition in partitions:
+        print(current_partition)
